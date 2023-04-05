e0eb6acf07750db6cb90a2db7f60102f2607a455
Usuario <davidcastillo1906@outlook.es>
2023-04-05 12:20:19 -0400

    migraciones personal

--- database/migrations/2023_04_05_153548_create_empleados_table.php
+++ database/migrations/2023_04_05_153548_create_empleados_table.php
@@ -13,6 +13,33 @@ return new class extends Migration
     {
         Schema::create('empleados', function (Blueprint $table) {
             $table->id();
+            $table->string('nombres')->unique();
+            $table->string('ap_paterno');
+            $table->string('ap_materno');
+            $table->string('avatar');
+            $table->string('nacionalidad');
+            $table->string('nro_documento');
+            $table->string('tipo_documento');
+            $table->string('ext_visa_laboral');
+            $table->string('email_personal');
+            $table->string('email_institucional');
+            $table->string('fecha_nacimiento');
+            $table->string('genero');
+            $table->string('estado_civil');
+            $table->string('telf_celular');
+            $table->string('telf_fijo');
+            $table->string('direccion');
+            $table->string('estado_registro');
+            $table->string('estado');
+            $table->string('nom_sis');
+            $table->string('num_suc');
+            $table->string('id_regional');
+            $table->string('id_sucursal');
+            $table->string('id_empresa');
+            $table->string('id_cargo');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('user_id')->references('id')->on('users');
             $table->timestamps();
         });
     }
